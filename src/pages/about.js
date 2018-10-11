6f3ebc3db79df7c8dc93e80568f9edbcdcd5f5a2
Justin Huang <justinh7@illinois.edu>
2018-10-11 01:03:33 -0500

    Tried unsuccessfully for 4 hours

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Route, Redirect } from 'react-router';
 
 class About extends React.Component {
   render() {
     return (
       <div>
         <h2>About</h2>
+        <button onClick={this.props.location.addToDo}> Snooze </button>
       </div>
     );
   }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,55 +1,42 @@
 import React, { Component } from "react";
+import ToDoListItem from "./components/toDoListItem";
+import { Route, Redirect } from 'react-router'
 
 class Home extends React.Component {
   state = {
-    newTodo: "",
     todos: [
       "Snooze",
       "Snooze again",
       "Wake up",
       "Rest eyes",
       "Accidentally fall back asleep",
-      "Wake up late"
+      "Wake up late",
+      "Eat breakfast",
+      "Fall back asleep again"
     ],
-    edits: []
   };
 
-  handleChange = todo => event => {
-    let updatedEdits = this.state.edits;
-    updatedEdits[this.state.todos.indexOf(todo)] = event.target.value;
+  addToDo = () => {
     this.setState({
-      edits: updatedEdits
-    });
-
-    console.log(this.state.edits);
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const index = this.state.todos.indexOf(event.target.value);
-    let newTodos = this.state.todos;
-    newTodos[index] = this.state.edits[index];
-    let updatedEdits = this.state.edits;
-    updatedEdits[index] = "";
-    this.setState({
-      todos: newTodos,
-      edits: updatedEdits
-    });
+      todos: [...this.state.todos, "Snooze"]
+    })
   };
 
   render() {
-    return ( <div>
+    return (
+      <div><div>
         <h2>Home</h2>
-        {this.state.todos.map(todo => (
-          <li key={todo}>
-            {todo + " "}
-            <input type="text" onChange={this.handleChange(todo)} />
-            <button value={todo} onClick={this.handleSubmit}>
-              Update
-            </button>
-          </li>
+        {this.state.todos.map(x => (
+          <ToDoListItem todo={x} />
         ))}
       </div>
+      <Redirect
+        to={{
+          pathname: "about",
+          search: "asdf",
+          state: { referrer: "home" }
+        }}
+      /></div>
     );
   }
 }
